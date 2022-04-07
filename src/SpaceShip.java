3948bd9ca556d551528cc68ede6ed1d1845c290b
gabriel1010 <101879808+ElCeacker@users.noreply.github.com>
2022-04-07 11:33:28 +0100

    Fix: Arreglo de contructor y getters-setters
    Se han quitado parámetros ya que solo pide 3 en el contructor y se han añadido los getters y setters correspondientes

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -5,21 +5,15 @@ public class SpaceShip {
     public float aceleracion;
     public float velocidadX;
     public float velocidadY;
-    public float coordenadaX;
-    public float coordenadaY;
+    public int coordenadaX;
+    public int coordenadaY;
     public int direccionX;
     public int direccionY;
 
-    public SpaceShip(String nombre, int matriculaGalactica, float aceleracion, float velocidadX, float velocidadY, float coordenadaX, float coordenadaY, int direccionX, int direccionY) {
+    public SpaceShip(String nombre, int matriculaGalactica, float aceleracion) {
         this.nombre = nombre;
         this.matriculaGalactica = matriculaGalactica;
         this.aceleracion = aceleracion;
-        this.velocidadX = velocidadX;
-        this.velocidadY = velocidadY;
-        this.coordenadaX = coordenadaX;
-        this.coordenadaY = coordenadaY;
-        this.direccionX = direccionX;
-        this.direccionY = direccionY;
     }
 
     public String getNombre() {
@@ -44,53 +38,5 @@ public class SpaceShip {
 
     public void setAceleracion(float aceleracion) {
         this.aceleracion = aceleracion;
-    }
-
-    public float getVelocidadX() {
-        return velocidadX;
-    }
-
-    public void setVelocidadX(float velocidadX) {
-        this.velocidadX = velocidadX;
-    }
-
-    public float getVelocidadY() {
-        return velocidadY;
-    }
-
-    public void setVelocidadY(float velocidadY) {
-        this.velocidadY = velocidadY;
-    }
-
-    public float getCoordenadaX() {
-        return coordenadaX;
-    }
-
-    public void setCoordenadaX(float coordenadaX) {
-        this.coordenadaX = coordenadaX;
-    }
-
-    public float getCoordenadaY() {
-        return coordenadaY;
-    }
-
-    public void setCoordenadaY(float coordenadaY) {
-        this.coordenadaY = coordenadaY;
-    }
-
-    public int getDireccionX() {
-        return direccionX;
-    }
-
-    public void setDireccionX(int direccionX) {
-        this.direccionX = direccionX;
-    }
-
-    public int getDireccionY() {
-        return direccionY;
-    }
-
-    public void setDireccionY(int direccionY) {
-        this.direccionY = direccionY;
     }
 }
